4931a5858eba89a0381503dff9d12ac6d9d074f9
Erhan Gök <erhan_gk08@outlook.com>
2023-12-11 17:54:37 +0300

    Update main.c

--- lab4_q1/Core/Src/main.c
+++ lab4_q1/Core/Src/main.c
@@ -6,7 +6,10 @@ uint32_t count = 0;
 uint32_t isr = 0;
 uint32_t tdr = 0;
 void UART_Init();
-void printChar(char c);
+void printChar(uint8_t c);
+int _print(int f,char *ptr, int len);
+void print(char *s);
+uint8_t uart_rx(void);
 void TimerInit();
 void TIM2_IRQHandler(void);
 
@@ -14,16 +17,17 @@ void TIM2_IRQHandler(void);
 int main()
 {
 	UART_Init();
-	TimerInit();
 	while(1)
 	{
+		print(uart_rx());
+
 	}
 }
 
 void UART_Init()
 {
-	RCC->IOPENR |= RCC_IOPENR_GPIOAEN; //Enable clock
-	RCC->APBENR1 |= RCC_APBENR1_USART2EN;
+	RCC->IOPENR |= RCC_IOPENR_GPIOAEN; //Enable clock for line A
+	RCC->APBENR1 |= RCC_APBENR1_USART2EN; //Enable clock for UART2
 
 	GPIOA->MODER &= ~GPIO_MODER_MODE2_Msk; //Clear MODE2
 	GPIOA->MODER |= GPIO_MODER_MODE2_1; //Set PA2 as alternate function
@@ -60,26 +64,48 @@ void TimerInit()
 
 void TIM2_IRQHandler(void){
 	TIM2->SR &= ~(1<<0); // Clear UIF update interrupt flag
-	printChar('a');
+
 	count++;
 }
 
 
-void printChar(char c)
+void printChar(uint8_t c)
 {
-	USART2->TDR = 'a';
+	USART2->TDR = c;
 	while(!(USART2->ISR & USART_ISR_TC))
 	{
 	}
 }
 
+int _print(int f,char *ptr, int len)
+{
+	(void)f;
+	for(int i=0; i<len; i++)
+	{
+		printChar(ptr[i]);
+	}
+	return len; // return length
+}
 
 
+void print(char *s)
+{
+	// count number of characters in s string until a null byte comes `\0`
+	uint8_t length = 0;
+	while(s[length]!='\0')
+	{
+		length++;
+	}
+	_print(0, s, length);
+}
 
-
-
-
-
+uint8_t uart_rx(void)
+{
+	while(!(USART2->ISR & USART_ISR_RXNE_RXFNE))
+	{
+	}
+	return (uint8_t) USART2->RDR;
+}
 
 
 
