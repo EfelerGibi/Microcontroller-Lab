8c0755122544969558d9f6cf2ea4c4c3d697a9ac
Efeler Gibi <43582815+EfelerGibi@users.noreply.github.com>
2023-12-16 19:30:21 +0300

    Added comments for l4q3

--- lab4_q3/Core/Src/main.c
+++ lab4_q3/Core/Src/main.c
@@ -3,12 +3,16 @@
 
 volatile uint8_t counter = 0;
 volatile uint32_t millis = 0;
+
 void delay_ms(uint32_t delay);
+
 void SysTickInit();
 
+// PWM function declarations
 void PWM_Init();
 void setDutyCycle(uint16_t dutyCycle);
 
+// UART initialization and functions
 void UART_Init();
 void printChar(uint8_t c);
 int _print(int f,char *ptr, int len);
@@ -16,24 +20,31 @@ void print(char *s);
 uint8_t uart_rx(void);
 void uart_tx(uint8_t c);
 
+// Constants for keypad
 #define A 50
 #define B A
 #define C A
 #define D A
-#define clear 10
-#define send 11
+#define clear 10 // "*" sign is for clearing the keypad
+#define send 11 // "#" confirms the entered value.
+
+// If this does not exist, counter gets discarded while optimizing
 uint8_t temp_counter;
+
+// 4x4 matrix for keypad
 int matrix[4][4] = {{1,2,3,A},
 					{4,5,6,B},
 					{7,8,9,C},
 					{clear,0,send,D}};
 
-uint8_t uart_transmit_flag = 0;
-uint8_t dutycycle_raw = 0;
-uint8_t dutycycle;
-uint8_t updateStatus();
+uint8_t dutycycle_raw = 0; // Raw duty cycle value
+uint8_t dutycycle;         // Processed duty cycle value
+uint8_t updateStatus();    // Function to update status of GPIO
+
 
 void init_keypad();
+
+// Function to scan the keypad
 void scan_keypad(uint8_t row);
 
 void EXTI0_1_IRQHandler(void);
@@ -41,32 +52,36 @@ void EXTI2_3_IRQHandler(void);
 void EXTI4_15_IRQHandler(void);
 
 int main(){
+    // Initialize UART, keypad, SysTick, and PWM
 	UART_Init();
 	init_keypad();
 	SysTickInit();
     PWM_Init();
-	char str_dutycycle[4];
+
+	char str_dutycycle[4]; // String to hold duty cycle
+
 	while(1){
-		utoa(dutycycle,str_dutycycle, 10);
+		utoa(dutycycle,str_dutycycle, 10); // Convert duty cycle to string
 		print("Current Duty Cycle: %");
 		for(int i=0;i<4;i++)
 		{
-			uart_tx(str_dutycycle[i]);
+			uart_tx(str_dutycycle[i]); // Transmit duty cycle character by character
 		}
 		print("\n\r");
 
 		delay_ms(2000);
-		temp_counter = counter;
+		temp_counter = counter; // Counter gets discarded if not used.
 
 	}
 	return 0;
 }
 
-
+// Initialize all the inputs and interrupts needed for keypad
 void init_keypad(){
-
+    // Enable clock for GPIOA and GPIOB
     RCC->IOPENR |= RCC_IOPENR_GPIOAEN | RCC_IOPENR_GPIOBEN;
 
+    // Configure GPIO pins for keypad
     GPIOA->MODER &= ~(GPIO_MODER_MODE9_Msk | GPIO_MODER_MODE8_Msk);
     GPIOA->MODER |= GPIO_MODER_MODE8_0;
 
@@ -81,132 +96,145 @@ void init_keypad(){
                     GPIO_MODER_MODE5_0 |
                     GPIO_MODER_MODE9_0;
 
-    // Set initial output levels
+    // Set initial output levels for GPIO pins to HIGH
     GPIOA->ODR |= GPIO_ODR_OD8;
     GPIOB->ODR |= GPIO_ODR_OD9 | GPIO_ODR_OD5 | GPIO_ODR_OD4;
 
-	GPIOA->PUPDR &= ~GPIO_PUPDR_PUPD9_Msk;
-	GPIOB->PUPDR &= ~GPIO_PUPDR_PUPD0_Msk | GPIO_PUPDR_PUPD2_Msk | GPIO_PUPDR_PUPD8_Msk;
+    // Setup pins as Pullup
+    GPIOA->PUPDR &= ~GPIO_PUPDR_PUPD9_Msk;
+    GPIOB->PUPDR &= ~GPIO_PUPDR_PUPD0_Msk | GPIO_PUPDR_PUPD2_Msk | GPIO_PUPDR_PUPD8_Msk;
 
-	GPIOA->PUPDR |= GPIO_PUPDR_PUPD9_1;
-	GPIOB->PUPDR |= GPIO_PUPDR_PUPD0_1 | GPIO_PUPDR_PUPD2_1 | GPIO_PUPDR_PUPD8_1;
-	RCC->APBENR2 |= (1U<<0); //APB peripheral clock enable register
+    GPIOA->PUPDR |= GPIO_PUPDR_PUPD9_1;
+    GPIOB->PUPDR |= GPIO_PUPDR_PUPD0_1 | GPIO_PUPDR_PUPD2_1 | GPIO_PUPDR_PUPD8_1;
 
-	EXTI->EXTICR[2] |= (0U << 8*1);
-	EXTI->EXTICR[0] |= (1U << 8*0);
-	EXTI->EXTICR[0] |= (1U << 8*2);
-	EXTI->EXTICR[2] |= (1U << 8*0);
+    // Enable APB peripheral clock
+    RCC->APBENR2 |= (1U<<0);
 
-	EXTI->RTSR1 |= (1U << 9);
-	EXTI->RTSR1 |= (1U << 0);
-	EXTI->RTSR1 |= (1U << 2);
-	EXTI->RTSR1 |= (1U << 8);
+    // Configure external interrupt lines for keypad
+    EXTI->EXTICR[2] |= (0U << 8*1);
+    EXTI->EXTICR[0] |= (1U << 8*0);
+    EXTI->EXTICR[0] |= (1U << 8*2);
+    EXTI->EXTICR[2] |= (1U << 8*0);
 
-	EXTI->IMR1 |= (1U<<9);
-	EXTI->IMR1 |= (1U<<0);
-	EXTI->IMR1 |= (1U<<2);
-	EXTI->IMR1 |= (1U<<8);
+    // Configure rising edge trigger for external interrupts
+    EXTI->RTSR1 |= (1U << 9);
+    EXTI->RTSR1 |= (1U << 0);
+    EXTI->RTSR1 |= (1U << 2);
+    EXTI->RTSR1 |= (1U << 8);
 
-	NVIC_SetPriority(EXTI0_1_IRQn, 2);
-	NVIC_EnableIRQ(EXTI0_1_IRQn);
+    // Enable external interrupts
+    EXTI->IMR1 |= (1U<<9);
+    EXTI->IMR1 |= (1U<<0);
+    EXTI->IMR1 |= (1U<<2);
+    EXTI->IMR1 |= (1U<<8);
 
-	NVIC_SetPriority(EXTI2_3_IRQn, 2);
-	NVIC_EnableIRQ(EXTI2_3_IRQn);
+    // Enable NVIC interrupts
+    NVIC_SetPriority(EXTI0_1_IRQn, 2);
+    NVIC_EnableIRQ(EXTI0_1_IRQn);
 
-	NVIC_SetPriority(EXTI4_15_IRQn, 2);
-	NVIC_EnableIRQ(EXTI4_15_IRQn);
+    NVIC_SetPriority(EXTI2_3_IRQn, 2);
+    NVIC_EnableIRQ(EXTI2_3_IRQn);
+
+    NVIC_SetPriority(EXTI4_15_IRQn, 2);
+    NVIC_EnableIRQ(EXTI4_15_IRQn);
 }
 
+// This function is for the logic for when keypad is pressed
 void scan_keypad(uint8_t row)
 {
-	delay_ms(20); //for debouncing
-	uint8_t column;
+    delay_ms(20); // Delay for debouncing
+    uint8_t column;
 
-	GPIOA->ODR &= ~GPIO_ODR_OD8;
-	if (!updateStatus()){
-		column = 0;
-	}
-	GPIOA->ODR |= GPIO_ODR_OD8;
+    // Check each column to identify the pressed key
+    // This turns off each row and checks if any of the rows turned off.
+    GPIOA->ODR &= ~GPIO_ODR_OD8;
+    if (!updateStatus()){
+        column = 0;
+    }
+    GPIOA->ODR |= GPIO_ODR_OD8; //then turns them on again.
 
-	GPIOB->ODR &= ~GPIO_ODR_OD9;
+    GPIOB->ODR &= ~GPIO_ODR_OD9;
+    if (!updateStatus()){
+        column = 1;
+    }
+    GPIOB->ODR |= GPIO_ODR_OD9;
 
-	if (!updateStatus()){
-		column = 1;
-	}
-	GPIOB->ODR |= GPIO_ODR_OD9;
+    GPIOB->ODR &= ~GPIO_ODR_OD5;
+    if (!updateStatus()){
+        column = 2;
+    }
+    GPIOB->ODR |= GPIO_ODR_OD5;
 
-	GPIOB->ODR &= ~GPIO_ODR_OD5;
-	if (!updateStatus()){
-		column = 2;
-	}
-	GPIOB->ODR |= GPIO_ODR_OD5;
+    GPIOB->ODR &= ~GPIO_ODR_OD4;
+    if (!updateStatus()){
+        column = 3;
+    }
+    GPIOB->ODR |= GPIO_ODR_OD4;
 
-	GPIOB->ODR &= ~GPIO_ODR_OD4;
-	if (!updateStatus()){
-		column = 3;
-	}
-	GPIOB->ODR |= GPIO_ODR_OD4;//if not first 3 column its the 4th column
-
-	uint8_t current_num = matrix[row-1][column];
-	if(current_num == send)
-	{
-		setDutyCycle(dutycycle_raw);
-		dutycycle = dutycycle_raw;
-		dutycycle_raw = 0;
-	}
-	else if(current_num == clear)
-	{
-		dutycycle_raw = 0;
-	}
-	else if(current_num < 10)
-	{
-			dutycycle_raw = 10*dutycycle_raw + current_num;
-		if(dutycycle_raw > 100)
-		{
-			dutycycle_raw=100;
-		}
-	}
+    // Lookup the matrix for what value is in that position
+    uint8_t current_num = matrix[row][column];
+    if(current_num == send)
+    {
+        setDutyCycle(dutycycle_raw); // Set PWM duty cycle
+        dutycycle = dutycycle_raw;   // Update duty cycle since raw duty cycle gets reset.
+        dutycycle_raw = 0;           // Reset raw duty cycle
+    }
+    else if(current_num == clear)// if clear is pressed, reset raw duty cycle
+    {
+        dutycycle_raw = 0;
+    }
+    else if(current_num < 10) //if a normal number is pressed, update the raw duty cycle
+    {
+        dutycycle_raw = 10*dutycycle_raw + current_num; // Update raw duty cycle
+        if(dutycycle_raw > 100)
+        {
+            dutycycle_raw=100; // Limit duty cycle to 100
+        }
+    }
 }
 
+// Read the status of the rows
 uint8_t updateStatus()
 {
-	return (GPIOA->IDR & GPIO_IDR_ID9) ||
-			(GPIOB->IDR & GPIO_IDR_ID0) ||
-			(GPIOB->IDR & GPIO_IDR_ID2) ||
-			(GPIOB->IDR & GPIO_IDR_ID8);
+    return (GPIOA->IDR & GPIO_IDR_ID9) ||
+           (GPIOB->IDR & GPIO_IDR_ID0) ||
+           (GPIOB->IDR & GPIO_IDR_ID2) ||
+           (GPIOB->IDR & GPIO_IDR_ID8);
 }
 
+// Interrupt handler for EXTI0_1
 void EXTI0_1_IRQHandler(void) {
-	counter++;
+    counter++; //to count how many times the interrupts are called
 
-    scan_keypad(2);
-    // Clear the interrupt flag
+    scan_keypad(1); // Scan keypad for row 2
     EXTI->RPR1 = EXTI_RPR1_RPIF0;  // Clear the pending bit for EXTI line 0
 }
 
+// Interrupt handler for EXTI2_3
 void EXTI2_3_IRQHandler(void) {
-	counter++;
+    counter++;//to count how many times the interrupts are called
 
-    scan_keypad(3);
-    // Clear the interrupt flag
-    EXTI->RPR1 = EXTI_RPR1_RPIF2;  // Clear the pending bit for EXTI line 0
+    scan_keypad(2); // Scan keypad for row 3
+    EXTI->RPR1 = EXTI_RPR1_RPIF2;  // Clear the pending bit for EXTI line 2
 }
 
+// Interrupt handler for EXTI4_15
 void EXTI4_15_IRQHandler(void) {
-	counter++;
+    counter++;//to count how many times the interrupts are called
 
-    // Clear the interrupt flag
+    // Determine which key was pressed and scan accordingly
     if((EXTI->RPR1&EXTI_RPR1_RPIF8))
     {
-    	scan_keypad(4);
+        scan_keypad(3); // Scan keypad for row 4
     }
     if((EXTI->RPR1&EXTI_RPR1_RPIF9))
     {
-    	scan_keypad(1);
+        scan_keypad(0); // Scan keypad for row 1
     }
-    EXTI->RPR1 |= EXTI_RPR1_RPIF8;
-    EXTI->RPR1 |= EXTI_RPR1_RPIF9;
+    EXTI->RPR1 |= EXTI_RPR1_RPIF8; // Clear the pending bit for EXTI line 8
+    EXTI->RPR1 |= EXTI_RPR1_RPIF9; // Clear the pending bit for EXTI line 9
 }
+
 
 void UART_Init()
 {
@@ -308,14 +336,14 @@ void PWM_Init() {
 
 
     GPIOA->AFR[1] &= ~(0xF << ((10 - 8) * 4));  // Clear the current AF setting for PA10
-    GPIOA->AFR[1] |= (2 << ((10 - 8) * 4));    // Set the correct AF (AF2) for TIM1_CH3 for PA10
+    GPIOA->AFR[1] |= (2 << ((10 - 8) * 4));    // Set the AF (AF2) for TIM1_CH3 for PA10
 
     TIM1->PSC = 1600 - 1;  // Prescaler for 1kHz PWM frequency
-    TIM1->ARR = 100;       // Auto-reload value for 100 steps
+    TIM1->ARR = 100;       // Auto-reload value for 100 steps, this is for 1% increments
     TIM1->CCMR2 |= TIM_CCMR2_OC3M_1 | TIM_CCMR2_OC3M_2; // PWM mode 1 on Channel 3
     TIM1->CCER |= TIM_CCER_CC3E;        // Enable capture/compare for channel 3
     TIM1->BDTR |= TIM_BDTR_MOE;         // Main output enable (needed for TIM1)
     TIM1->CR1 |= TIM_CR1_CEN;           // Enable timer
-    setDutyCycle(0);
+    setDutyCycle(0); //set initial duty cycle to 0
 }
 
