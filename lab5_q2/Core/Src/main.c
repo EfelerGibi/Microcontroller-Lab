a20d346e587607b7d3c73b95b2b6e06b8d45e737
Beyzanur Cam <115371982+beyzanrcam@users.noreply.github.com>
2024-01-06 16:03:18 +0300

    lab5_q22

--- lab5_q2/Core/Src/main.c
+++ lab5_q2/Core/Src/main.c
@@ -1,175 +1,140 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2024 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
-/* Includes ------------------------------------------------------------------*/
-#include "main.h"
+#include "stm32g0xx.h"
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
+#define TIM_AutoReload 16000//1600
+uint16_t counter = 0;
+int D1;
+int D2;
+int D3;
+int D4;
 
-/* USER CODE END Includes */
+volatile uint32_t millis = 0;
 
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
+void SysTickInit();
+void SysTick_Handler();
+void ButtonInit();
+void setPanel();
+void PanelInit();
+void setDigit(uint32_t mask,int digit);
 
-/* USER CODE END PTD */
 
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
+static const uint8_t digitPins[16] = {
+    0x3F, // 0
+    0x06, // 1
+    0x5B, // 2
+    0x4F, // 3
+    0x66, // 4
+    0x6D, // 5
+    0x7D, // 6
+    0x07, // 7
+    0x7F, // 8
+    0x6F, // 9
+    0x77, // A
+    0x7C, // B
+    0x39, // C
+    0x5E, // D
+    0x79, // E
+    0x71  // F
+};
 
-/* USER CODE END PD */
 
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
 
-/* USER CODE END PM */
+int main(void) {
+	ButtonInit();
+	PanelInit();
+	SysTickInit();
 
-/* Private variables ---------------------------------------------------------*/
+	setDigit(GPIO_ODR_OD6,0);
 
-/* USER CODE BEGIN PV */
-
-/* USER CODE END PV */
-
-/* Private function prototypes -----------------------------------------------*/
-void SystemClock_Config(void);
-/* USER CODE BEGIN PFP */
-
-/* USER CODE END PFP */
-
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
-
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
-{
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
-  SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
-  /* USER CODE BEGIN 2 */
-
-  /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
-  }
-  /* USER CODE END 3 */
+    while(1) {
+    }
+    return 0;
 }
 
-/**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /** Configure the main internal regulator output voltage
-  */
-  HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1);
+void SysTickInit() {
+    SysTick->CTRL |= SysTick_CTRL_ENABLE_Msk;   // Enable SysTick
+    SysTick->LOAD = 16000;                      // Load 16000 for 1ms tick
+    SysTick->VAL = 0;                           // Reset SysTick
+    SysTick->CTRL |= SysTick_CTRL_TICKINT_Msk;  // SysTick Enable Interrupt
+    SysTick->CTRL |= SysTick_CTRL_CLKSOURCE_Msk; // SysTick clock source = AHB
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSIDiv = RCC_HSI_DIV1;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    NVIC_EnableIRQ(SysTick_IRQn);
+    NVIC_SetPriority(SysTick_IRQn, 0);
 }
 
-/* USER CODE BEGIN 4 */
-
-/* USER CODE END 4 */
-
-/**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
+void delay_ms(uint32_t delay) {
+    millis = 0;
+    while (millis < delay)
+    {
+        // Wait for the specified duration
+    }
+}
+void SysTick_Handler(void) {
+    millis++; // Increment millis value
+}
+void setDigit(uint32_t mask,int digit)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
-  /* USER CODE END Error_Handler_Debug */
+    //Disable D1,D2,D3,D4
+    GPIOA->ODR |= GPIO_ODR_OD1;
+	GPIOA->ODR |= GPIO_ODR_OD4;
+	GPIOA->ODR |= GPIO_ODR_OD5;
+	GPIOA->ODR |= GPIO_ODR_OD6;
+
+    GPIOA->ODR &= ~mask; //Enable mask2
+    GPIOB->ODR |= digitPins[digit];
+    GPIOB->ODR &= digitPins[digit];
 }
 
-#ifdef  USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
+void ButtonInit()
 {
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+	RCC->IOPENR |= RCC_IOPENR_GPIOAEN_Msk;
+	GPIOA->MODER &= ~GPIO_MODER_MODE0_Msk;
+	GPIOA->PUPDR &= ~GPIO_PUPDR_PUPD0_Msk;
+	GPIOA->PUPDR |=  (1U<<0);
+
+	RCC->APBENR2 |= (1U<<0);
+	EXTI->RTSR1 |= (1U<<0);
+	EXTI->IMR1 |= (1U<<0);
+
+    EXTI->FTSR1 |= (1U<<0);        // Enable EXTI on Falling edge
+	EXTI->RTSR1 &= ~(1U<<0);       // Disable EXTI on Rising edge
+
+	NVIC_SetPriority(EXTI0_1_IRQn,0);
+	NVIC_EnableIRQ(EXTI0_1_IRQn);
 }
-#endif /* USE_FULL_ASSERT */
+
+void EXTI0_1_IRQHandler(void){
+	EXTI->FPR1 |= (1<<0); //EXTI falling edge pending register 1 (EXTI_FPR1)
+	counter = 0;
+	GPIOC->ODR &= ~(1U << 6);
+}
+
+void PanelInit()
+{
+	RCC->IOPENR |= RCC_IOPENR_GPIOBEN_Msk;
+    GPIOB->MODER &= ~0xFFFF;
+    GPIOB->MODER |= 0x5555;
+    GPIOB->ODR |= digitPins[0];
+
+
+    GPIOA->MODER &= ~GPIO_MODER_MODE1_Msk;
+    GPIOA->MODER |= GPIO_MODER_MODE1_0;
+
+    GPIOA->MODER &= ~GPIO_MODER_MODE4_Msk;
+    GPIOA->MODER |= GPIO_MODER_MODE4_0;
+
+    GPIOA->MODER &= ~GPIO_MODER_MODE5_Msk;
+    GPIOA->MODER |= GPIO_MODER_MODE5_0;
+
+    GPIOA->MODER &= ~GPIO_MODER_MODE6_Msk;
+    GPIOA->MODER |= GPIO_MODER_MODE6_0; //pa1 pa4 pa5 pa6
+
+    //Disable D1,D2,D3,D4
+    GPIOA->ODR |= GPIO_ODR_OD1;
+	GPIOA->ODR |= GPIO_ODR_OD4;
+	GPIOA->ODR |= GPIO_ODR_OD5;
+	GPIOA->ODR |= GPIO_ODR_OD6;
+}
+
+
+
